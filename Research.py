7fcc14a385eef64fc76194557c8353a5f7713003
Tung, Phien Phin Jeff <pptung@illumina.com>
2020-10-27 20:26:19 +0800

    Initial version

---
+++ Research.py
@@ -0,0 +1,78 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D, BatchNormalization
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator
+from keras import backend as K
+
+img_width, img_height = 48, 48
+  
+train_data_dir = 'drive/My Drive/Research file/train'
+validation_data_dir = 'drive/My Drive/Research file/test'
+
+nb_train_samples = 302
+nb_validation_samples = 34
+epochs = 1000
+batch_size = 32
+
+if K.image_data_format() == 'channels_first':
+  input_shape = (3, img_width, img_height)
+else:
+  input_shape = (img_width, img_height, 3)
+
+train_datagen = ImageDataGenerator(
+    rescale = 1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size = batch_size,
+    class_mode = 'binary'
+)
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size = batch_size,
+    class_mode = 'binary'
+)
+
+
+model = Sequential()
+model.add(Conv2D(64,(3,3), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D((2,2)))
+
+model.add(Conv2D(128,(3,3), activation='relu'))
+model.add(Conv2D(128,(3,3), activation='relu'))
+model.add(MaxPooling2D((2,2)))
+
+model.add(Conv2D(256,(3,3), activation='relu'))
+model.add(Conv2D(256,(3,3), activation='relu'))
+model.add(MaxPooling2D((2,2)))
+
+model.add(Flatten())
+model.add(Dense(4096, activation='relu'))
+model.add(Flatten())
+model.add(Dense(4096, activation='relu'))
+model.add(Flatten())
+model.add(Dense(1, activation='softmax'))
+
+model.compile(loss='binary_crossentropy',
+             optimizer='rmsprop',
+             metrics=['accuracy'])
+
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size
+)
+
+model.save_weights('first_try.h5')
