47bb9c26746dda8d9497ba70e5ae7dc5c9db0513
jefftung1991 <Jefftung1991@gmail.com>
2020-11-30 13:23:35 +0800

    Update Research.py

--- Research.py
+++ Research.py
@@ -3,16 +3,24 @@ from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D, BatchNorm
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
+from tensorflow.python.client import device_lib
+import tensorflow as tf
+import keras
+
+
+print(device_lib.list_local_devices()) 
+
+sess = tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True)) 
 
 img_width, img_height = 48, 48
   
-train_data_dir = 'drive/My Drive/Research file/train'
-validation_data_dir = 'drive/My Drive/Research file/test'
+train_data_dir = 'train'
+validation_data_dir = 'test'
 
-nb_train_samples = 302
-nb_validation_samples = 34
+nb_train_samples = 494
+nb_validation_samples = 55
 epochs = 1000
-batch_size = 32
+batch_size = 64
 
 if K.image_data_format() == 'channels_first':
   input_shape = (3, img_width, img_height)
@@ -60,14 +68,14 @@ model.add(Dense(4096, activation='relu'))
 model.add(Flatten())
 model.add(Dense(4096, activation='relu'))
 model.add(Flatten())
-model.add(Dense(1, activation='softmax'))
+model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy',
              optimizer='rmsprop',
              metrics=['accuracy'])
 
 
-model.fit_generator(
+model.fit(
     train_generator,
     steps_per_epoch=nb_train_samples // batch_size,
     epochs=epochs,
@@ -75,4 +83,4 @@ model.fit_generator(
     validation_steps=nb_validation_samples // batch_size
 )
 
-model.save_weights('first_try.h5')
+model.save('Asian_First_Try.h5')
